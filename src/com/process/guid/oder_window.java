e1d84292eb73478cf8dcf2dab188d97937fae5da
tonyisTHEenemy <tsttony85@gmail.com>
2019-04-12 18:03:22 +0300

    Initial commit

---
+++ src/com/process/guid/oder_window.java
@@ -0,0 +1,43 @@
+package com.process.guid;
+
+import javafx.scene.layout.StackPane;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class stPaneBasic extends Application {
+
+private StackPane basicPane{
+
+    @Override
+           public void start(Stage primaryStage) throws Exception{
+
+            VBox rootStartBox = new VBox();
+
+            //StackPane
+            virtPane = new StackPane();
+
+            //Add label StackPane
+            Label pgLabel = new Label("It's Label");
+
+
+        }
+
+
+
+
+    }
+
+
+}
